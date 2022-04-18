b6d9f88f0e57f5ff6ca34f2f7bd5b366626c3a7e
IlmHe <ilmar.r.heinonen@gmail.com>
2022-04-18 15:01:27 +0300

    indexin html,css,js tehty

--- FrontEnd/JS/index.js
+++ FrontEnd/JS/index.js
@@ -1,13 +1,37 @@
 'use strict';
-
-const text = `
+//Populate header
+const headerText = `
 <nav id="navLinks">
-<ul>
+<ul class="headingFont">
 <li> <a href="front.html"> Koti</a> </li>
 <li> <a href="front.html"> Koti2</a> </li>
 <li> <a href="front.html"> Koti3</a> </li>
 </ul>
-<button>Login</button>
-</nav>`;
+</nav>
+<button class="headingFont">Login</button>
+`;
 
-document.querySelector('.headerClass').innerHTML = text;
+document.querySelector('header').innerHTML = headerText;
+
+//Populate footer
+const footerText = `
+<p class = "footerP textFont">Support: admin@metropolia.fi</p>
+<p class = "footerP textFont">Feedback: admin2@metropolia.fi</p>
+<p class = "footerP textFont">Want to buy us more legos? Paypal</p>
+`;
+
+document.querySelector('footer').innerHTML = footerText;
+
+//Populate right div
+const rightDivText = `<h3 class ="headingFont" id ="userNameText"> Usernameasddddddddddddddd </h3>
+<p><img src="https://ideascdn.lego.com/media/generate/lego_ci/bcf52b26-a2b1-4975-8c38-59efa86eb254/resize:900:600/legacy" id="profilePic"></p>
+<p class = "textFont" id ="divText"> all ultimate gurus believe each other, only great teachers have a volume.  </p>`;
+
+document.querySelector('.right').innerHTML = rightDivText;
+
+//Populate left div
+const leftDivText = `<h3 class ="headingFont" id ="userNameText"> Usernameasddddddddddddddd </h3>
+<p><img src="https://ideascdn.lego.com/media/generate/lego_ci/bcf52b26-a2b1-4975-8c38-59efa86eb254/resize:900:600/legacy" id="profilePic"></p>
+<p class = "textFont" id ="divText"> all ultimate gurus believe each other, only great teachers have a volume.  </p>`;
+
+document.querySelector('.left').innerHTML = leftDivText;
